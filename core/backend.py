74ba465c15e03dd122de6282afe714d2795972ed
ihjhuo <41447049+ihjhuo@users.noreply.github.com>
2018-09-18 19:54:01 -0700

    Update backend.py

--- core/backend.py
+++ core/backend.py
@@ -80,21 +80,20 @@ class ModelWrapper(object):
         detected = face_cascade.detectMultiScale(gray_img, 1.1)
         dd=[]
         for i in range(len(detected)):
-            detected.append({'box':detected[i]})
+            dd.append({'box':detected[i]})
         detected=dd
         faces = np.empty((len(detected), self.img_size, self.img_size, 3))
         for i, d in enumerate(detected):
-            if d['confidence'] > 0.95:
-                x1, y1, w, h = d['box']
-                x2 = x1 + w
-                y2 = y1 + h
-                xw1 = max(int(x1 - ad * w), 0)
-                yw1 = max(int(y1 - ad * h), 0)
-                xw2 = min(int(x2 + ad * w), img_w - 1)
-                yw2 = min(int(y2 + ad * h), img_h - 1)
-                cv2.rectangle(input_img, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                # cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
-                faces[i, :, :, :] = cv2.resize(input_img[yw1:yw2 + 1, xw1:xw2 + 1, :], (self.img_size, self.img_size))
+            x1, y1, w, h = d['box']
+            x2 = x1 + w
+            y2 = y1 + h
+            xw1 = max(int(x1 - ad * w), 0)
+            yw1 = max(int(y1 - ad * h), 0)
+            xw2 = min(int(x2 + ad * w), img_w - 1)
+            yw2 = min(int(y2 + ad * h), img_h - 1)
+            cv2.rectangle(input_img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            # cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
+            faces[i, :, :, :] = cv2.resize(input_img[yw1:yw2 + 1, xw1:xw2 + 1, :], (self.img_size, self.img_size))
 
         if len(detected) > 0:
             with self.graph.as_default():
@@ -103,7 +102,6 @@ class ModelWrapper(object):
         # prediction results with BBX & AGES
         pred_res = []
         for i, d in enumerate(detected):
-            if d['confidence'] > 0.8:
-                pre_age=predicted_ages[i].astype(int)
-                pred_res.append([{'box': d['box'], 'age':pre_age}])
+            pre_age=predicted_ages[i].astype(int)
+            pred_res.append([{'box': d['box'], 'age':pre_age}])
         return pred_res
